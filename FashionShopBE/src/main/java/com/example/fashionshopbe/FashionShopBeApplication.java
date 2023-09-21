cf2a2e99d50157827072183fd13984e3cc7bccc9
AnNgoIT <ngothuaan2002@gmail.com>
2023-09-22 04:52:13 +0700

    fix version Spring Boot to 3.1.4

--- FashionShopBE/src/main/java/com/example/fashionshopbe/FashionShopBeApplication.java
+++ FashionShopBE/src/main/java/com/example/fashionshopbe/FashionShopBeApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FashionShopBeApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FashionShopBeApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(FashionShopBeApplication.class, args);
+    }
 
 }
--- FashionShopBE/src/test/java/com/example/fashionshopbe/FashionShopBeApplicationTests.java
+++ FashionShopBE/src/test/java/com/example/fashionshopbe/FashionShopBeApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class FashionShopBeApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
