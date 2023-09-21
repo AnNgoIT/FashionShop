88634b41af077c1fd9e3145d919c761806c52e5c
AnNgoIT <ngothuaan2002@gmail.com>
2023-09-21 12:41:41 +0700

    Config BackEnd Base

---
+++ FashionShopBE/src/main/java/com/example/fashionshopbe/FashionShopBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.fashionshopbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FashionShopBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FashionShopBeApplication.class, args);
+	}
+
+}
---
+++ FashionShopBE/src/test/java/com/example/fashionshopbe/FashionShopBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fashionshopbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FashionShopBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
